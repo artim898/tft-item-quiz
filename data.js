d840a7bffeeb740b9cafc834e35752457df45064
artim898 <timchanpt2022@gmail.com>
2026-02-19 20:59:13 +0800

    add data

---
+++ data.js
@@ -0,0 +1,34 @@
+const tftData = {
+  components: [
+    { id: "bf_sword", name: "B.F. Sword" },
+    { id: "recurve_bow", name: "Recurve Bow" },
+    { id: "chain_vest", name: "Chain Vest" },
+    { id: "negatron_cloak", name: "Negatron Cloak" },
+    { id: "giants_belt", name: "Giant's Belt" },
+    { id: "needlessly_large_rod", name: "Needlessly Large Rod" },
+    { id: "tear", name: "Tear of the Goddess" },
+    { id: "sparring_gloves", name: "Sparring Gloves" }
+  ],
+  combined: [
+    {
+      id: "giant_slayer",
+      name: "Giant Slayer",
+      components: ["bf_sword", "recurve_bow"]
+    },
+    {
+      id: "gargoyle_stoneplate",
+      name: "Gargoyle Stoneplate",
+      components: ["chain_vest", "negatron_cloak"]
+    },
+    {
+      id: "bloodthirster",
+      name: "Bloodthirster",
+      components: ["bf_sword", "negatron_cloak"]
+    },
+    {
+      id: "hextech_gunblade",
+      name: "Hextech Gunblade",
+      components: ["bf_sword", "needlessly_large_rod"]
+    }
+  ]
+};
