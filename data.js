25b1fc0b671eab31e3d44cee4b30e1b9a1e31873
artim898 <timchanpt2022@gmail.com>
2026-02-19 21:52:04 +0800

    Update data.js

--- data.js
+++ data.js
@@ -7,36 +7,34 @@ const tftData = {
     { id: "giants_belt", name: "Giant's Belt" },
     { id: "needlessly_large_rod", name: "Needlessly Large Rod" },
     { id: "tear", name: "Tear of the Goddess" },
-    { id: "sparring_gloves", name: "Sparring Gloves" }
-    { id: "frying_pan", name: "Frying Pan" },
-    { id: "giants_belt", name: "Giant's Belt" },
     { id: "sparring_gloves", name: "Sparring Gloves" },
+    { id: "frying_pan", name: "Frying Pan" }
   ],
-combined: [
-  {
-    id: "giant_slayer",
-    name: "Giant Slayer",
-    components: ["bf_sword", "recurve_bow"]
-  },
-  {
-    id: "deathblade",
-    name: "Deathblade",
-    components: ["bf_sword", "chain_vest"]
-  },
-  {
-    id: "gargoyle_stoneplate",
-    name: "Gargoyle Stoneplate",
-    components: ["chain_vest", "negatron_cloak"]
-  },
-  {
-    id: "red_buff",
-    name: "Red Buff",
-    components: ["giants_belt", "frying_pan"]
-  },
-  {
-    id: "rabadons_deathcap",
-    name: "Rabadon's Deathcap",
-    components: ["needlessly_large_rod", "needlessly_large_rod"]
-  }
-]
+  combined: [
+    {
+      id: "giant_slayer",
+      name: "Giant Slayer",
+      components: ["bf_sword", "recurve_bow"]
+    },
+    {
+      id: "deathblade",
+      name: "Deathblade",
+      components: ["bf_sword", "chain_vest"]
+    },
+    {
+      id: "gargoyle_stoneplate",
+      name: "Gargoyle Stoneplate",
+      components: ["chain_vest", "negatron_cloak"]
+    },
+    {
+      id: "red_buff",
+      name: "Red Buff",
+      components: ["giants_belt", "frying_pan"]
+    },
+    {
+      id: "rabadons_deathcap",
+      name: "Rabadon's Deathcap",
+      components: ["needlessly_large_rod", "needlessly_large_rod"]
+    }
+  ]
 };
